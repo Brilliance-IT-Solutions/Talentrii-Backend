d6e568fe702adcc26ae93e2b4cdf6899a45ae38f
Sonam <sonam.brillianceit@gmail.com>
2024-02-23 17:46:51 +0530

    router js changed

--- server/router.js
+++ server/router.js
@@ -28,7 +28,7 @@ const saveChallenge = require("../server/controllersDB/saved/saveChallengeDB")
 const editChallengeDB = require("../server/controllersDB/challenge/editChallengeDB")
 
 router.get('/', function (req, res, next) {
-    res.send('SERVER STARTED', DB_NAME='b76kvbmwwyvn66ik9frb');
+    res.send('SERVER STARTED', process.env.DB_NAME);
 })
 
 
